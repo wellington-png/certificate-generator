a95a96d67e43b3ecb8dab8565ab2a0631e0deb53
wellington-png <wellington.santos.na@gmail.com>
2022-10-31 23:56:09 -0300

    add cert_config

--- certificado.py
+++ certificado.py
@@ -1,19 +1,20 @@
 from PIL import Image, ImageDraw, ImageFont
 import PIL
+from certicado_config import CertificadoConfig
 
 
-class Certificado(object):
+class Certificado(CertificadoConfig):
+    line_signature = "_______________________________"
+
     def __init__(
-        self,
-        filename_or_size_or_Image,
-        mode="RGBA",
-        background=(0, 0, 0, 0),
-        encoding="utf8",
+        self, filename="img6.jpg", mode="RGBA", background=(0, 0, 0, 0), encoding="utf8"
     ):
-        if isinstance(filename_or_size_or_Image, str):
-            self.filename = filename_or_size_or_Image
-            self.image = Image.open(self.filename)
-            self.size = self.image.size
+        super().__init__(mode, background, encoding)
+        self.filename = filename
+        self.image = Image.open(self.filename)
+        self.size = self.image.size
+        self.background = background
+        self.mode = mode
         self.draw = ImageDraw.Draw(self.image)
         self.encoding = encoding
 
@@ -22,125 +23,56 @@ class Certificado(object):
 
     def show(self):
         self.image.show()
-    
+
     def get_size(self):
         return self.image.size
 
-    def get_font_size(self, text, font, max_width=None, max_height=None):
-        if max_width is None and max_height is None:
-            raise ValueError("You need to pass max_width or max_height")
-        font_size = 1
-        text_size = self.get_text_size(font, font_size, text)
-        if (max_width is not None and text_size[0] > max_width) or (
-            max_height is not None and text_size[1] > max_height
-        ):
-            raise ValueError("Text can't be filled in only (%dpx, %dpx)" % text_size)
-        while True:
-            if (max_width is not None and text_size[0] >= max_width) or (
-                max_height is not None and text_size[1] >= max_height
-            ):
-                return font_size - 1
-            font_size += 1
-            text_size = self.get_text_size(font, font_size, text)
+    def create_main_text(self, text, font_size=40, font="freemomo/FreeMono.ttf", stronk=0):
+        self.write_text_box(
+            (230, 800),
+            text,
+            box_width=2000,
+            font_filename=font,
+            font_size=font_size,
+            color=self.background,
+            place="justify",
+        )
 
-    def write_text(
-        self,
-        xy,
-        text,
-        font_filename,
-        font_size=11,
-        color=(0, 0, 0),
-        max_width=None,
-        max_height=None,
-    ):
-        x, y = xy
-        if font_size == "fill" and (max_width is not None or max_height is not None):
-            font_size = self.get_font_size(text, font_filename, max_width, max_height)
-        text_size = self.get_text_size(font_filename, font_size, text)
-        font = ImageFont.truetype(font_filename, font_size)
-        if x == "center":
-            x = (self.size[0] - text_size[0]) / 2
-        if y == "center":
-            y = (self.size[1] - text_size[1]) / 2
-        self.draw.text((x, y), text, font=font, fill=color)
-        return text_size
+    def create_date_city(self, text):
+        x, y = self.get_text_size(font="freemomo/FreeMono.ttf", font_size=40, text=text)
+        pos = 2500 // 2
+        k = x // 2
+        pos = pos - k
+        self.write_text_box(
+            (pos, 1100),
+            box_width=912,
+            font_filename="freemomo/FreeMono.ttf",
+            font_size=40,
+            color=self.background,
+            text=text,
+        )
+    
+    def left_signature(self, nome, cargo, instituicao):
+        self.write_text_box((400, 1300), box_width=600, font_filename="freemomo/FreeMono.ttf", font_size=40, color=self.background, text=self.line_signature, place='center')
+        self.write_text_box((400, 1350), box_width=600, font_filename="freemomo/FreeMono.ttf", font_size=40, color=self.background, text=nome, place='center')
+        self.write_text_box((400, 1400), box_width=600, font_filename="freemomo/FreeMono.ttf", font_size=40, color=self.background, text=cargo, place='center')
+        self.write_text_box((400, 1450), box_width=600, font_filename="freemomo/FreeMono.ttf", font_size=40, color=self.background, text=instituicao, place='center')
 
-    def get_text_size(self, font_filename, font_size, text):
-        font = ImageFont.truetype(font_filename, font_size)
-        return font.getsize(text)
+    def right_signature(self, nome, cargo, instituicao):
+        self.write_text_box((1500, 1300), box_width=600, font_filename="freemomo/FreeMono.ttf", font_size=40, color=self.background, text=self.line_signature, place='center')
+        self.write_text_box((1500, 1350), box_width=600, font_filename="freemomo/FreeMono.ttf", font_size=40, color=self.background, text=nome, place='center')
+        self.write_text_box((1500, 1400), box_width=600, font_filename="freemomo/FreeMono.ttf", font_size=40, color=self.background, text=cargo, place='center')
+        self.write_text_box((1500, 1450), box_width=600, font_filename="freemomo/FreeMono.ttf", font_size=40, color=self.background, text=instituicao, place='center')
 
 
-
-
-    def write_text_box(
-        self,
-        xy,
-        text,
-        box_width,
-        font_filename,
-        font_size=11,
-        color=(0, 0, 0),
-        place="justify",
-        justify_last_line=False,
-        position="top",
-        line_spacing=1.0,
-    ):
-        x, y = xy
-        lines = []
-        line = []
-        words = text.split()
-        for word in words:
-            new_line = " ".join(line + [word])
-            size = self.get_text_size(font_filename, font_size, new_line)
-            text_height = size[1] * line_spacing
-            last_line_bleed = text_height - size[1]
-            if size[0] <= box_width:
-                line.append(word)
-            else:
-                lines.append(line)
-                line = [word]
-        if line:
-            lines.append(line)
-        lines = [" ".join(line) for line in lines if line]
-
-        if position == "middle":
-            height = (self.size[1] - len(lines) * text_height + last_line_bleed) / 2
-            height -= text_height  # the loop below will fix this height
-        elif position == "bottom":
-            height = self.size[1] - len(lines) * text_height + last_line_bleed
-            height -= text_height  # the loop below will fix this height
-        else:
-            height = y
-
-        for index, line in enumerate(lines):
-            height += text_height
-            if place == "justify":
-                words = line.split()
-                if (index == len(lines) - 1 and not justify_last_line) or len(
-                    words
-                ) == 1:
-                    self.write_text((x, height), line, font_filename, font_size, color)
-                    continue
-                line_without_spaces = "".join(words)
-                total_size = self.get_text_size(
-                    font_filename, font_size, line_without_spaces
-                )
-                space_width = (box_width - total_size[0]) / (len(words) - 1.0)
-                start_x = x
-                for word in words[:-1]:
-                    self.write_text(
-                        (start_x, height), word, font_filename, font_size, color
-                    )
-                    word_size = self.get_text_size(font_filename, font_size, word)
-                    start_x += word_size[0] + space_width
-                last_word_size = self.get_text_size(font_filename, font_size, words[-1])
-                last_word_x = x + box_width - last_word_size[0]
-                self.write_text(
-                    (last_word_x, height), words[-1], font_filename, font_size, color
-                )
-            elif place == 'center':
-                total_size = self.get_text_size(font_filename, font_size, line)
-                x_left = int(x + ((box_width - total_size[0]) / 2))
-                self.write_text((x_left, height), line, font_filename,
-                                font_size, color)
-        return (box_width, height - y)
+if __name__ == '__main__':
+    t = Certificado()
+    t.create_main_text("""Certificamos que Fulana Santos da Silva, CPF 101.555.413-59, participou, como CURSISTA, do Projeto de
+extensão contínuo ROBÓTICA COMO UMA INTERVENÇÃO SOCIAL: ROBÓTICA VOLTADO AO AGRO,
+coordenado por FELIPE GONÇALVES DOS SANTOS, selecionado através do EDITAL No 03/2020/PROEN/IFS -
+PROGRAMA INSTITUCIONAL DE PESQUISA MULTIDISCIPLINAR DE APOIO AO ENSINO, no período de
+29/08/2022 a 16/09/2022, com uma carga horária total de 40 horas.""", 40)
+    t.create_date_city('Corrente - PI, 16 de Setembro de 2022.')
+    t.right_signature('Jose Wellington', 'Cordenador', 'IFPI campos corrente')
+    t.left_signature('Jose Wellington', 'Cordenador', 'IFPI campos corrente')
+    t.show()
